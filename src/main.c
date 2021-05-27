055f5d8493c7ae25e39830c49a8f7ded805f1bfb
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-05-27 13:17:49 +0200

    Talf to my main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
-#include "server.h"
+#include "adminui.h"
+#include "logger.h"
 #include "pilot.h"
+#include "robot.h"
 
 /**
  * Fonction Main de Robot_Commando
@@ -8,9 +10,14 @@
  */
 int main (int argc, char *argv[])
 {
-    Server* server = Server_new(Pilot_new());
-	Server_start(server);
-	Server_stop(server);
-	Server_free(server);
+	Robot* robot = Robot_new();
+	Logger* logger = Logger_new(robot);
+	Pilot* pilot = Pilot_new(logger, robot);
+	AdminUI* adminUI = AdminUI_new(logger, pilot);
+
+	AdminUI_free(adminUI);
+	Pilot_free(pilot);
+	Logger_free(logger);
+	Robot_free(robot);
 	return 0;
 }
